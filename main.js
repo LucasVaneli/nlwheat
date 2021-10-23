26c7e68b0a2519c963993aadf153ea7297ae2ac1
willianguerra <willianguerrapossamai0901@gmail.com>
2021-10-23 20:12:51 -0300

    Finalizado aula 4

---
+++ main.js
@@ -0,0 +1,31 @@
+const LinksSocialMedia = {
+  github: 'LucasVaneli',
+  instagram: 'vanelizinn',
+  facebook: 'lucas.vaneli.9',
+  twitter: 'LucasVaneli'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://www.${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfo() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      UserImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfo()
